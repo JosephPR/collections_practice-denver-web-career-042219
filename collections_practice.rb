eff07d36daa84f64b089201f1262385c74c1c460
Joseph Reilly <josephpr@Josephs-MacBook-Air.local>
2019-04-25 20:06:00 -0600

    yep

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,13 @@
+require 'pry'
 
+def get_first_name_of_season_winner(data, season)
+      binding.pry
+  # puts "Hi"
+  # data[season].each do |info|
+  #
+  #   if info["status"] == "Winner"
+  #   end
+  # end
+end
+
+get_first_name_of_season_winner(data, "season 10")
